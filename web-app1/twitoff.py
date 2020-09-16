a967f15b46478717d4bcd455e9b79d07c87761f4
shemaaali <shemaali8787@gmail.com>
2020-09-16 18:30:42 -0500

    Added a prediction funtion

--- web-app1/twitoff.py
+++ web-app1/twitoff.py
@@ -2,6 +2,7 @@
 #import the flask
 from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
 from .twitter import add_or_update_user, insert_example_users
 from os import getenv
 # create function for the app
@@ -32,6 +33,18 @@ def create_app():
             message = "Error addding {}: {}".format(name, e)
             tweets=[]
         return render_template('user.html', title=name, tweets=tweets, message=message)
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                              request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)    
     #associated with update button
     @app.route('/update')
     def update():
